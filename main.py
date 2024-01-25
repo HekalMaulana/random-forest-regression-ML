079ab066e5cd5e3588e2b0adeae9a6e40dff31ac
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-25 18:17:36 +0800

    Predict the result and visulising a random forest regression

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ print(y)
 
 # Training random forest regression
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(random_state=0)
+regressor = RandomForestRegressor(random_state=0, n_estimators=10)
 regressor.fit(X,y)
 
 # Predicting the result
 y_predict = regressor.predict([[6]])
+print(y_predict)
+
+# Visualing the random forest regression with high resolution
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title("Truth Or Bluff (Random Forest Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
 
